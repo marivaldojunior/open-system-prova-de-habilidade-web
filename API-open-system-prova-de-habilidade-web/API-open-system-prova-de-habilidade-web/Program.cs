c02a3b54e091e6f7adedacf28f4708075f143722
marivaldojunior <marivaldo.pereira@ufba.br>
2022-05-09 11:48:21 -0300

    criando projeto no Visual Studio 2019

---
+++ API-open-system-prova-de-habilidade-web/API-open-system-prova-de-habilidade-web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace API_open_system_prova_de_habilidade_web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
