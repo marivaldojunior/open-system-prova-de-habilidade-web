a333a8277321c55139163aea3bba6f92c925a69c
marivaldojunior <marivaldo.pereira@ufba.br>
2022-05-11 12:46:08 -0300

    dependencias

--- SugestoesAPI/Model/Sugestao.cs
+++ SugestoesAPI/Model/Sugestao.cs
@@ -2,10 +2,12 @@
 
 public class Sugestao
 {
-	public int Id { get; set; }
+    public int Id { get; set; }
 
+    [StringLength(50)]
 	public string NomeColaborador { get; set; } = string.Empty;
 
+	[StringLength(300)]
 	public string Comentario { get; set; } = string.Empty;
 
 	public int DepartamentoId { get; set; }
