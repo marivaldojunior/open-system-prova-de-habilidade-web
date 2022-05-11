d6e55ffa2f893bb1534b343fb08e312621d3090f
marivaldojunior <marivaldo.pereira@ufba.br>
2022-05-11 13:33:53 -0300

    initial create migrations

---
+++ SugestoesAPI/Migrations/20220511163247_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SugestoesAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sugestoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeColaborador = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Comentario = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    DepartamentoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sugestoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sugestoes_Departamentos_DepartamentoId",
+                        column: x => x.DepartamentoId,
+                        principalTable: "Departamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sugestoes_DepartamentoId",
+                table: "Sugestoes",
+                column: "DepartamentoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sugestoes");
+
+            migrationBuilder.DropTable(
+                name: "Departamentos");
+        }
+    }
+}
