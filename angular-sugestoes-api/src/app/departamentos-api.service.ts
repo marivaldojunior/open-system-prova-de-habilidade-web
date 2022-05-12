60225e07593ef648362ac85932bd636559603bd7
marivaldojunior <marivaldo.pereira@ufba.br>
2022-05-12 15:32:09 -0300

    adicionando get,post,put,delete nos services

--- angular-sugestoes-api/src/app/departamentos-api.service.ts
+++ angular-sugestoes-api/src/app/departamentos-api.service.ts
@@ -7,8 +7,21 @@ import { Observable } from 'rxjs/internal/Observable';
 })
 export class DepartamentosApiService {
   readonly departamentosAPIUrl = "https://localhost:7233/api";
+
   constructor(private http: HttpClient) { }
+
   getDepartamentoList():Observable<any[]>{
-    return this.http.get<any>(this.departamentosAPIUrl + '/Departamentoes')
+    return this.http.get<any>(this.departamentosAPIUrl + '/Departamentoes');
+  }
+
+  addDepartamento(data:any){
+    return this.http.post(this.departamentosAPIUrl + '/Departamentoes', data);
+  }
+
+  updateDepartamento(id:number|string, data:any){
+    return this.http.put(this.departamentosAPIUrl + `/Departamentoes/${id}`, data);
+  }
+  deleteDepartamento(id:number|string){
+    this.http.delete(this.departamentosAPIUrl + `/Departamentoes/${id}`);
   }
 }
--- angular-sugestoes-api/src/app/sugestoes-api.service.ts
+++ angular-sugestoes-api/src/app/sugestoes-api.service.ts
@@ -6,11 +6,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SugestoesApiService {
-  readonly sugestoesAPIUrl = "https://localhost:7233/api";
+  readonly sugestoesAPIUrl = "https://localhost:7233/api"; 
+
   constructor(private http: HttpClient) { }
+
   getSugestaoList():Observable<any[]>{
-    return this.http.get<any>(this.sugestoesAPIUrl + '/Sugestaos')
+    return this.http.get<any>(this.sugestoesAPIUrl + '/Sugestaos');
   }
 
-  
+  addSugestao(data:any){
+    return this.http.post(this.sugestoesAPIUrl + '/Sugestaos', data);
+  }
+  updateSugestao(id:number|string, data:any){
+    return this.http.put(this.sugestoesAPIUrl + `/Sugestaos/${id}`, data);
+  }
+  deleteSugestao(id:number|string){
+    this.http.delete(this.sugestoesAPIUrl + `/Sugestaos/${id}`);
+  }
 }
